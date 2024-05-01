0cfa4711c7e79dd824011653c6dbf73e7b486158
erin collective <solarpunk.erin@gmail.com>
2024-05-01 12:48:32 +0200

    Add solution and project from maui-blazor app template.

---
+++ source/maui/Yarml.Ui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Yarml.Ui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
